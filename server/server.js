1675851272a39c11296cc055878e9fff73725701
roma <silvervaza@gmail.com>
2019-02-16 22:42:54 +0200

    Frost bit

---
+++ server/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const uuid = require('uuid/v4');
+const session = require('express-session')
+const FileStore = require('session-file-store')(session);
+const bodyParser = require('body-parser');
+const LocalStrategy = require('passport-local').Strategy;
+const users = [{
+  id:'2f24vvg', email: "rest@gustav.com", password: 'password'
+}]
+passposrt.use(new LocalStrategy(
+    {usernameField: 'email'},
+    (email, password, done) => {
+      console.log('Inside local strategy callback')
+      const user = users[0]
+      if(email === user.email && password === user.password) {
+        console.log('Local strategy returned true')
+        return done(null, user)
+      }
+    }
+));
+passport.serializeUser((user, done) => {
+  console.log('Inside serializeUser callback .User id is save to the session file store here')
+  done(null, user.id)
+})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+  genid: (req) => {
+    console.log('Inside the session middleware genid function')
+    console.log(`Request object sessionID from client: ${req.sessionID}`)
+    return uuid()
+  },
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+app.get('/', (req, res) => {
+  console.log("in router get")
+  console.log('Inside the homepage callback function')
+  console.log(req.sessionID)
+  res.send(`You hit home page\n`)
+})
+app.get('/login', (req, res) => {
+    console.log('Inside GET /login callback function')
+    console.log(req.sessionID)
+    res.send(`you got the login page\n`)
+})
+app.post('/login', (req, res) => {
+    console.log('Inside POST /login callback function')
+    passport.authenticate('local', (err, user, info ) => {
+      console.log('Inside passport.authenticate() callback');
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      console.log(`req.user: ${JSON.stringify(req.user)}`)
+      req.login(user, (err) => {
+        console.log('Inside req.login() callback')
+        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+        console.log(`req.user: ${JSON.stringify(req.user)}`)
+        return res.send('You were authenticated & logged in !\n');
+      })
+    })(req, res, next);
+})
+app.listen(3000, () => {
+  console.log('Listening on localhost:3000')
+})
