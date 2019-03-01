b00cfbf31deb477558ef4d5208c3feb3718020e3
roma <silvervaza@gmail.com>
2019-03-01 21:20:42 +0100

    search session object

--- server/server.js
+++ server/server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const uuid = require('uuid/v4')
 const app = express();
-app.use(cookieParser());
 app.use(bodyParser());
+var expiryDate = new Date(Date.now() + 60*1000)
+app.use(session({
+	genid: (req) => {
+		console.log('inside the session middleware')
+		console.log(req.sessionID)
+		return uuid()
+	},
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true
+}))
 app.get('/', (req, res,next) => {
-  
-  res.send('Returning with some text')
+  res.send('Returning with some text\n')
 })
 app.get('/login', (req, res,next) => {
-  res.send('You have come at home')
+  res.send('You have come at home\n')
 })
 
 app.post('/login', (req, res,next) => {
-  console.dir(req.body)
-  res.send('You sent a post request')
+  console.log("have contact")
+  console.dir(req)
+  res.send('You sent a post request\n')
 })
 
 
